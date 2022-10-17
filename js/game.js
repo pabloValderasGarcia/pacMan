d9d8eb33d886ea9f1b4f786392f374de22993f63
Pablo Valderas Garcia <pvalgarn@gmail.com>
2022-10-17 19:23:05 +0200

    fantasmas un poco bug

--- js/game.js
+++ js/game.js
@@ -26,10 +26,13 @@ game.Tablero = class {
     ]
 
     generarTablero() {
+
         for (let i = 0; i < this.board.length; i++) {
             document.getElementById('fila' + i).textContent = "";
         }
+        
         document.getElementById('fila')
+
         for (let i = 0; i < this.board.length; i++) {
             for (let j = 0; j < this.board[i].length; j++) {
                 let td = document.createElement('td');
@@ -64,6 +67,7 @@ game.Tablero = class {
                 document.getElementById('fila' + i).appendChild(td);
             }
         }
+
     }
 
 }
@@ -76,161 +80,153 @@ game.Pacman = class {
     }
 
     generarPacMan() {
-        this.tablero.board[17][15] = '1';
+        this.tablero.board[17][15] = 1;
         this.tablero.generarTablero();
-    }
-
-    posicionPacMan() {
-        let posicionPacMan = "";
-        for (let i = 0; i < this.tablero.board.length; i++) {
-            for (let j = 0; j < this.tablero.board[i].length; j++) {
-                if (this.tablero.board[i][j] == '1') {
-                    posicionPacMan = i + " " + j;
-                    return posicionPacMan;
-                }
-            }
-        }
+        // COORDENADAS PACMAN
+        this.x = 15;
+        this.y = 17;
     }
 
     movimientoPacMan() {
 
+        // TECLAS
         window.addEventListener("keydown", (event) => {
-            let posicion = this.posicionPacMan().split(' ');
+            fantasma.iniciarFantasmas();
             switch (event.code) {
+                // TECLA ARRIBA
                 case "ArrowUp":
                     console.log("UP");
-                    posicion = this.posicionPacMan().split(' ');
-                    if (this.tablero.board[posicion[0] - 1][posicion[1]] != '╚' &&
-                        this.tablero.board[posicion[0] - 1][posicion[1]] != '╗' &&
-                        this.tablero.board[posicion[0] - 1][posicion[1]] != '═' &&
-                        this.tablero.board[posicion[0] - 1][posicion[1]] != '║' &&
-                        this.tablero.board[posicion[0] - 1][posicion[1]] != '╦' &&
-                        this.tablero.board[posicion[0] - 1][posicion[1]] != '╝') {
-                        this.tablero.board[posicion[0] - 1][posicion[1]] = '1';
-                        this.tablero.board[posicion[0]][posicion[1]] = ' ';
+                    let up = this.tablero.board[this.y-1][this.x];
+                    if (up == '•' || up == '●' || up == ' ') {
+                        this.tablero.board[this.y-1][this.x] = 1;
+                        this.tablero.board[this.y][this.x] = ' ';
+                        this.y--;
                     }
                     this.tablero.generarTablero();
                     break;
+                // TECLA IZQUIERDA
                 case "ArrowLeft":
                     console.log("LEFT");
-                    posicion = this.posicionPacMan().split(' ');
-                    if (this.tablero.board[posicion[0]][posicion[1] - 1] != '╗' &&
-                        this.tablero.board[posicion[0]][posicion[1] - 1] != '═' &&
-                        this.tablero.board[posicion[0]][posicion[1] - 1] != '║' &&
-                        this.tablero.board[posicion[0]][posicion[1] - 1] != '╝') {
-                        this.tablero.board[posicion[0]][posicion[1] - 1] = '1';
-                        this.tablero.board[posicion[0]][posicion[1]] = ' ';
+                    let left = this.tablero.board[this.y][this.x-1];
+                    if (left == '•' || left == '●' || left == ' ') {
+                        this.tablero.board[this.y][this.x-1] = 1;
+                        this.tablero.board[this.y][this.x] = ' ';
+                        this.x--;
                     }
                     this.tablero.generarTablero();
                     break;
+                // TECLA ABAJO
                 case "ArrowDown":
                     console.log("DOWN");
-                    posicion = this.posicionPacMan().split(' ');
-                    if (this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╗' &&
-                        this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '═' &&
-                        this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '║' &&
-                        this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╦' &&
-                        this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╔') {
-                        this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] = '1';
-                        this.tablero.board[posicion[0]][posicion[1]] = ' ';
+                    let down = this.tablero.board[this.y+1][this.x];
+                    if (down == '•' || down == '●' || down == ' ') {
+                        this.tablero.board[this.y+1][this.x] = 1;
+                        this.tablero.board[this.y][this.x] = ' ';
+                        this.y++;
                     }
                     this.tablero.generarTablero();
                     break;
+                // TECLA DERECHA
                 case "ArrowRight":
                     console.log("RIGHT");
-                    posicion = this.posicionPacMan().split(' ');
-                    if (this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '╚' &&
-                        this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '═' &&
-                        this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '║' &&
-                        this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '╔' &&
-                        this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != 1) {
-                        this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] = '1';
-                        this.tablero.board[posicion[0]][posicion[1]] = ' ';
+                    let right = this.tablero.board[this.y][this.x+1];
+                    if (right == '•' || right == '●' || right == ' ') {
+                        this.tablero.board[this.y][this.x+1] = 1;
+                        this.tablero.board[this.y][this.x] = ' ';
+                        this.x++;
                     }
                     this.tablero.generarTablero();
                     break;
             }
         })
 
-        // ARRIBA
-        document.getElementById("up").onclick = () => {
-            console.log("UP");
-            let posicion = this.posicionPacMan().split(' ');
-            if (this.tablero.board[posicion[0] - 1][posicion[1]] != '╚' &&
-                this.tablero.board[posicion[0] - 1][posicion[1]] != '╗' &&
-                this.tablero.board[posicion[0] - 1][posicion[1]] != '═' &&
-                this.tablero.board[posicion[0] - 1][posicion[1]] != '║' &&
-                this.tablero.board[posicion[0] - 1][posicion[1]] != '╦' &&
-                this.tablero.board[posicion[0] - 1][posicion[1]] != '╝') {
-                this.tablero.board[posicion[0] - 1][posicion[1]] = '1';
-                this.tablero.board[posicion[0]][posicion[1]] = ' ';
-            }
-            this.tablero.generarTablero();
-        }
-
-        // IZQUIERDA
-        document.getElementById("left").onclick = () => {
-            console.log("LEFT");
-            let posicion = this.posicionPacMan().split(' ');
-            if (this.tablero.board[posicion[0]][posicion[1] - 1] != '╗' &&
-                this.tablero.board[posicion[0]][posicion[1] - 1] != '═' &&
-                this.tablero.board[posicion[0]][posicion[1] - 1] != '║' &&
-                this.tablero.board[posicion[0]][posicion[1] - 1] != '╝') {
-                this.tablero.board[posicion[0]][posicion[1] - 1] = '1';
-                this.tablero.board[posicion[0]][posicion[1]] = ' ';
-            }
-            this.tablero.generarTablero();
-        }
-
-        // DERECHA
-        document.getElementById("down").onclick = () => {
-            console.log("DOWN");
-            let posicion = this.posicionPacMan().split(' ');
-            if (this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╗' &&
-                this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '═' &&
-                this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '║' &&
-                this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╦' &&
-                this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] != '╔') {
-                this.tablero.board[parseInt(posicion[0]) + 1][posicion[1]] = '1';
-                this.tablero.board[posicion[0]][posicion[1]] = ' ';
-            }
-            this.tablero.generarTablero();
-        }
-
-        // ABAJO
-        document.getElementById("right").onclick = () => {
-            console.log("RIGHT");
-            let posicion = this.posicionPacMan().split(' ');
-            if (this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '╚' &&
-                this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '═' &&
-                this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '║' &&
-                this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != '╔' &&
-                this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] != 1) {
-                this.tablero.board[posicion[0]][parseInt(posicion[1]) + 1] = '1';
-                this.tablero.board[posicion[0]][posicion[1]] = ' ';
-            }
-            this.tablero.generarTablero();
-        }
-
     }
 
 }
 
 // FANTASMA
-game.Fantasma = class {}
+game.Fantasma = class {
+    
+    constructor(tablero, pacMan) {
+        this.tablero = tablero;
+        this.pacMan = pacMan;
+    }
+
+    generarFantasmas() {
+        this.coordsFantasmas = [
+            //x, y, fantasma
+            [14, 8],
+            [16, 8],
+            [14, 7],
+            [16, 7]
+        ]
+        this.coordsFantasmas.forEach(coord => {
+            this.tablero.board[coord[1]][coord[0]] = 9;
+        });
+        this.tablero.generarTablero();
+    }
+
+    movimientoFantasmas() {
+        this.coordsFantasmas.forEach(coord => {
+            let up = this.tablero.board[coord[1]-1][coord[0]];
+            let left = this.tablero.board[coord[1]][coord[0]-1];
+            let down = this.tablero.board[coord[1]+1][coord[0]];
+            let right = this.tablero.board[coord[1]][coord[0]+1];
+
+            // LEFT Y RIGHT
+            if (this.pacMan.x < coord[0]) {
+                if (left == '•' || left == '●' || left == ' ' && left != 9) {
+                    this.tablero.board[coord[1]][coord[0]-1] = 9;
+                    this.tablero.board[coord[1]][coord[0]] = ' ';
+                    coord[0]--;
+                }
+                
+            } else {
+                if (right == '•' || right == '●' || right == ' ' && right != 9) {
+                    this.tablero.board[coord[1]][coord[0]+1] = 9;
+                    this.tablero.board[coord[1]][coord[0]] = ' ';
+                    coord[0]++;
+                }
+            }
+
+            // UP Y DOWN
+            if (this.pacMan.y < coord[1]) {
+                if (up == '•' || up == '●' || up == ' ' && up != 9) {
+                    this.tablero.board[coord[1]-1][coord[0]] = 9;
+                    this.tablero.board[coord[1]][coord[0]] = ' ';
+                    coord[1]--;
+                }
+            } else {
+                if (down == '•' || down == '●' || down == ' ' && down != 9) {
+                    this.tablero.board[coord[1]+1][coord[0]] = 9;
+                    this.tablero.board[coord[1]][coord[0]] = ' ';
+                    coord[1]++;
+                }
+            }
+        });
+    }
+
+    iniciarFantasmas() {
+        this.movimientoFantasmas();
+        this.tablero.generarTablero();
+    }
+
+}
 
 // JUEGO
 game.Juego = class {
 
-    constructor(tablero, pacMan) {
+    constructor(tablero, pacMan, fantasma) {
         this.tablero = tablero;
         this.pacMan = pacMan;
+        this.fantasma = fantasma;
     }
 
     jugar() {
         this.tablero.generarTablero();
         this.pacMan.generarPacMan();
         this.pacMan.movimientoPacMan();
+        this.fantasma.generarFantasmas();
     }
 
 }
--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 let tablero = new game.Tablero();
 let pacMan = new game.Pacman(tablero);
-let juego = new game.Juego(tablero, pacMan);
+let fantasma = new game.Fantasma(tablero, pacMan);
+let juego = new game.Juego(tablero, pacMan, fantasma);
 juego.jugar();
